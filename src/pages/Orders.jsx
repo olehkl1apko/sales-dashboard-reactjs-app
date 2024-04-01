bf4993253dd004e4e338e05db98291fa242a793d
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-01 17:40:50 +0300

    ADD: orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { Space, Table, Typography } from "antd";
 
-const Orders = () => {
-  return <div>Orders</div>;
-};
+import { Error } from "../components";
+import { useGetData } from "../hooks";
+
+function Orders() {
+  const { data, error, loading } = useGetData("/carts/1");
+
+  if (error) <Error />;
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Orders</Typography.Title>
+      <Table
+        loading={loading}
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "DiscountedPrice",
+            dataIndex: "discountedPrice",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Total",
+            dataIndex: "total",
+          },
+        ]}
+        dataSource={data?.products.map((product) => ({
+          ...product,
+          key: product.id,
+        }))}
+        pagination={{
+          pageSize: 10,
+        }}
+      ></Table>
+    </Space>
+  );
+}
 
 export default Orders;
